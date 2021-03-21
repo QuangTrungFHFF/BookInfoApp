5bab53d022bfaf5f3d60a067f560d53ccb3b4542
Tran Quang Trung <tranquangtrung.vgu@gmail.com>
2021-03-21 23:26:34 +0100

    Update splash screen

--- app/src/main/java/com/example/bookinfoapp/MainActivity.java
+++ app/src/main/java/com/example/bookinfoapp/MainActivity.java
@@ -13,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
 
         Button btnStart = (Button)findViewById(R.id.main_button);
         btnStart.setOnClickListener(new View.OnClickListener() {
